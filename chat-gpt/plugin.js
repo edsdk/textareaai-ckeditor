2222fa05667e6b6e11f7738ffd2fa0066531c24b
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2024-09-23 17:47:08 +0400

    2024.09

--- chat-gpt/plugin.js
+++ chat-gpt/plugin.js
@@ -6,112 +6,135 @@
  */
 
 
-//
-//   HOW TO INSTALL THIS ADD-ON
-//
-//   1. Copy the plugin as "ckeditor/plugins/chat-gpt/plugin.js"
-//   2. Add "chat-gpt" into "extraPlugins" config option
-//   3. See configuration samples on our GitHub page:
-//      https://github.com/edsdk/txt42-ckeditor
-//
+/*
+ *   HOW TO INSTALL THIS ADD-ON
+ *
+ *   1. Copy the plugin into
+ *
+ *        ckeditor/
+ *          plugins/
+ *            chat-gpt/
+ *              plugin.js
+ *
+ *   2. Add "chat-gpt" into "extraPlugins" config option
+ *   3. Done
+ *
+ *
+ *   VISUAL CONFIGURATION
+ *
+ *   Configure Txt42 visually using the Dashboard:
+ *
+ *       https://txt42.com/dashboard
+ *
+ *   Get there an API key to link with your installation.
+ *   Specify it as a CKEditor parameter:
+ *
+ *       apiKey: "..."
+ */
 
-var apiKey = CKEDITOR.config.apiKey;
-var version = CKEDITOR.config.version;
-var n1edPrefix = CKEDITOR.config.n1edPrefix;
-var n1edHttps = CKEDITOR.config.n1edHttps;
-var n1edPrefixApp = CKEDITOR.config.n1edPrefixApp;
-var n1edHttpsApp = CKEDITOR.config.n1edHttpsApp;
-var urlCache = CKEDITOR.config.urlCache;
-window.n1edPluginVersion=202308002;
-for (var i=0; i<Object.keys(CKEDITOR.instances).length; i++) {
-    var id = Object.keys(CKEDITOR.instances)[i];
-    if (CKEDITOR.instances[id].config.apiKey)
-        apiKey = CKEDITOR.instances[id].config.apiKey;
-    else if (CKEDITOR.instances[id].config.Flmngr && CKEDITOR.instances[id].config.Flmngr.apiKey)
-        apiKey = CKEDITOR.instances[id].config.Flmngr.apiKey;
-    if (CKEDITOR.instances[id].config.version)
-        version = CKEDITOR.instances[id].config.version;
-    if (CKEDITOR.instances[id].config.n1edPrefix)
-        n1edPrefix = CKEDITOR.instances[id].config.n1edPrefix;
-    if ("n1edHttps" in CKEDITOR.instances[id].config)
-        n1edHttps = CKEDITOR.instances[id].config.n1edHttps;
-    if (CKEDITOR.instances[id].config.n1edPrefixApp)
-        n1edPrefixApp = CKEDITOR.instances[id].config.n1edPrefixApp;
-    if ("n1edHttpsApp" in CKEDITOR.instances[id].config)
-        n1edHttpsApp = CKEDITOR.instances[id].config.n1edHttpsApp;
-    if (CKEDITOR.instances[id].config.urlCache)
-        urlCache = CKEDITOR.instances[id].config.urlCache;
-}
+(function() {
 
-// Cookies may contain data for development purposes (which version to load, from where, etc.).
-function getCookie(name) {
-    if (typeof window[name] !== 'undefined')
-        return "" + window[name];
-    var value = '; ' + document.cookie;
-    var parts = value.split('; ' + name + '=');
-    if (parts.length === 2)
-        return parts.pop().split(';').shift();
-    else
-        return null;
-}
-window.CKEDITOR_OVERRIDE_API_KEY_PARAM = "OVERRIDE_API_KEY";
-apiKey = getCookie("N1ED_APIKEY") || window.OVERRIDE_API_KEY || apiKey || window.N1ED_API_KEY || "TXAIAUTO";
-n1edHttps = (getCookie("N1ED_HTTPS") === "false" || n1edHttps === false) ? false : true;
-n1edPrefix = getCookie("N1ED_PREFIX") || n1edPrefix || null;
-n1edHttpsApp = (getCookie("N1ED_HTTPS_APP") === "false" || n1edHttpsApp === false) ? false : true;
-n1edPrefixApp = getCookie("N1ED_PREFIX_APP") || n1edPrefixApp || null;
-var protocol = n1edHttps ? "https" : "http";
-var host = (n1edPrefix ? (n1edPrefix + ".") : "") + "cloud.n1ed.com";
-version = getCookie("N1ED_VERSION") || version || "latest";
+    var PLUGIN_NAME = "chat-gpt";
+    var DEFAULT_API_KEY = "TXAI24RR1234123412341234";
 
-if (!urlCache) {
-    window.N1ED_PREFIX = n1edPrefix;
-    window.N1ED_HTTPS = n1edHttps;
-}
-window.N1ED_PREFIX_APP = n1edPrefixApp;
-window.N1ED_HTTPS_APP = n1edHttpsApp;
+    window.n1edPluginVersion=202409001;
+    window.n1edPluginEditor="ckeditor";
 
-var urlPlugin = (
-    urlCache ? (urlCache + apiKey + "/" + version) : (protocol + "://" + host + "/cdn/" + apiKey + "/" + version)
-) + "/ckeditor/plugins/N1EDEco/plugin.js";
+    function get(varName, defaultValue) {
+        if (window[varName] !== undefined && window[varName] !== "-")
+            return window[varName];
+        else
+            return defaultValue;
+    }
 
-var oldScriptLoaderLoad = window.CKEDITOR.scriptLoader.load;
-window.CKEDITOR.scriptLoader.load = function(scriptUrl, callback, scope, showBusy) {
-    return oldScriptLoaderLoad.apply(scope, [
-        scriptUrl,
-        function(completed, failed) {
-            if (!!failed && failed.length) {
-                console.error( '[CKEDITOR.resourceManager.load] Resource was not found at "' + failed[0] );
-
-                for (var i=0; i<failed.length; i++) {
-                    var m = failed[i].match(/^https?:\/\/cloud\.n1ed.com\/cdn\/[^/]+?\/[^/]+?\/ckeditor\/plugins\/([^/]+?)\/plugin\.js.*?/);
-                    if (m != null) {
-                        var pluginName = m[1];
-                        console.log("Using a stub for '" + pluginName + "' plugin");
-                        CKEDITOR.plugins.add(pluginName, {});
-                        if (!completed)
-                            completed = [];
-                        completed.push(failed[i]);
-                    }
-                }
-                failed = [];
+    var apiKey = CKEDITOR.config.apiKey;
+    if (!apiKey) {
+        for (var i = 0; i < Object.keys(CKEDITOR.instances).length; i++) {
+            var id = Object.keys(CKEDITOR.instances)[i];
+            if (CKEDITOR.instances[id].config.apiKey)
+                apiKey = CKEDITOR.instances[id].config.apiKey;
+            if (!apiKey) {
+                if (CKEDITOR.instances[id].config.Flmngr && CKEDITOR.instances[id].config.Flmngr.apiKey)
+                    apiKey = CKEDITOR.instances[id].config.Flmngr.apiKey;
             }
+        }
+    }
 
-            var elsLoaders = document.querySelectorAll(".n1ed_loading");
-            if (!!elsLoaders)
-                for (var i=0; i<elsLoaders.length; i++)
-                    elsLoaders.item(i).parentElement.removeChild(elsLoaders.item(i));
+    window.CKEDITOR_OVERRIDE_API_KEY_PARAM = "OVERRIDE_API_KEY";
+    apiKey = window.OVERRIDE_API_KEY || window.N1ED_API_KEY || apiKey || DEFAULT_API_KEY;
 
-            callback.apply(scope, [completed, failed]);
-        },
-        scope,
-        showBusy
-    ]);
-};
+    var version = get("N1ED_VERSION", "latest");
+    var n1edPrefix = get("N1ED_PREFIX", null);
+    var n1edHttps = get("N1ED_HTTPS", true);
 
-CKEDITOR.plugins.addExternal("N1EDEco", urlPlugin);
-CKEDITOR.plugins.add( "chat-gpt", {
-    "requires": ["N1EDEco"], // We can not move N1EDEco in this file due to we need to dynamically
-                             // embed configuration from your Dashboard into it.
-                             // So N1EDEco add-on can be loaded only from CDN
-});
+    var protocol = n1edHttps ? "https" : "http";
+
+    var host = (n1edPrefix ? (n1edPrefix + ".") : "") + "cloud.n1ed.com";
+    var urlPlugin = protocol + "://" + host + "/a/" + apiKey + "/plugins/N1EDEco/plugin.js";
+
+    var oldScriptLoaderLoad = window.CKEDITOR.scriptLoader.load;
+    window.CKEDITOR.scriptLoader.load = function(scriptUrl, callback, scope, showBusy) {
+        return oldScriptLoaderLoad.apply(scope, [
+            scriptUrl,
+            function(completed, failed) {
+                if (!!failed && failed.length) {
+                    console.error( '[CKEDITOR.resourceManager.load] Resource was not found at "' + failed[0] );
+
+                    for (var i=0; i<failed.length; i++) {
+                        var m = failed[i].match(/^https?:\/\/cloud\.n1ed\.com\/cdn\/[^/]+?\/[^/]+?\/ckeditor\/plugins\/([^/]+?)\/plugin\.js.*?/);
+
+                        if (!m) {
+                            m = failed[i].match(/^https?:\/\/cloud\.n1ed\.com\/.*\/plugin\.js.*?/);
+                        }
+                        if (m != null) {
+                            var pluginName = m[1];
+                            console.log("Using a stub for '" + pluginName + "' plugin");
+                            CKEDITOR.plugins.add(pluginName, {});
+                            if (!completed)
+                                completed = [];
+                            completed.push(failed[i]);
+                        }
+                    }
+                    failed = [];
+                }
+
+                var elsLoaders = document.querySelectorAll(".n1ed_loading");
+                if (!!elsLoaders)
+                    for (var i=0; i<elsLoaders.length; i++)
+                        elsLoaders.item(i).parentElement.removeChild(elsLoaders.item(i));
+
+                callback.apply(scope, [completed, failed]);
+            },
+            scope,
+            showBusy
+        ]);
+    };
+
+    CKEDITOR.plugins.addExternal("N1EDEco", urlPlugin);
+    CKEDITOR.plugins.add( PLUGIN_NAME, {
+        "requires": ["N1EDEco"], // We can not move Ecosystem in this file due to we need to dynamically
+        // embed configuration from your Dashboard into it.
+        // So Ecosystem add-on can be loaded only from CDN
+    });
+
+    function applyPatch() {
+        window.applyPatch && window.applyPatch(PLUGIN_NAME, window.n1edPluginVersion, apiKey);
+    }
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = protocol + "://" + host + "/static/pluginPatch.js";
+    document.head.appendChild(script);
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState === "loaded" || script.readyState === "complete") {
+                script.onreadystatechange = null;
+                applyPatch();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            applyPatch();
+        };
+    }
+
+})()
